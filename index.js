314c5d86911608af68a6e47df957c797fa00ae09
Ankush_agrawal <ankushagrawal4321@gmail.com>
2023-08-19 10:50:42 +0530

    mistake for not getting direct msg

--- index.js
+++ index.js
@@ -41,7 +41,8 @@ const server = app.listen(process.env.PORT,()=>{
 
 const io = socket(server,{
     cors:{
-        origin:"http://localhost:3000",
+        // origin:"http://localhost:3000",
+        origin:"https://ankushsnappy.vercel.app",
         credentials: true,
     }
 })
